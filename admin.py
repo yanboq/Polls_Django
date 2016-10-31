389b4a701612aebcd6d003b61a137e1958e0411b
Yanbo <syphone@163.com>
2016-10-30 22:34:19 -0400

    Django

---
+++ admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from .models import Question, Choice
+# Register your models here.
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    # fields = ['pub_date', 'question_text']
+
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
